6d884c2be7d977cc61d6232e286d513b6014c7b0
Vladimir Myullyari <Vladimir.Myullyari@lexisnexis.com>
2018-09-26 18:29:13 -0400

    Temp: save ch3/q03

---
+++ CCInterview/chapter03/q03_stack_of_plates/SetOfStacks.java
@@ -0,0 +1,14 @@
+public class SetOfStacks {
+
+    private int capacity;
+
+    private ArrayList
+
+    public SetOfStacks(int capacity) {
+        // check for positive number
+        this.capacity = capacity;
+    }
+
+    public void push(int value) {
+    }
+}
